6e3abe4465a75545a8c5229b1494fbb45859fb8c
laverka <valeriy.tovstyk@gmail.com>
2020-03-18 23:43:56 +0200

    change port to 5000

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const Koa =  require('koa');
+const Koa = require('koa');
 const app = new Koa();
+
 const mainRouter = require('./routes/index');
 
 app.use(mainRouter);
 
-app.listen(3000, () => console.log('Server running on https://localhost:3000'));
+app.listen(process.env.PORT || 5000, () => console.log('Server running on 5000 port'));
