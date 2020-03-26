7877e4995e13a64d535520f2a0d3cda9113c50eb
laverka <valeriy.tovstyk@gmail.com>
2020-03-26 22:17:12 +0200

    add mysql

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('mysql://sql7329583:yA3qUMJBIZ@sql7.freemysqlhosting.net:3306/mysql_task');
+const sequelize = new Sequelize('mysql://sql7329583:yA3qUMJBIZ@sql7.freemysqlhosting.net:3306/');
 // const sequelize = new Sequelize('mysql://root:pass@localhost:3308/mysql_task');
 
 sequelize
